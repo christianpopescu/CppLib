58ac4b8871fd5f16efa90a67667861b3d5715ac8
Christian Popescu <christian.popescu@outlook.com>
2019-12-13 09:19:38 +0100

    Create StringBuilder class

---
+++ CppStdLib/src/StringBuilder.h
@@ -0,0 +1,14 @@
+#ifndef CPP_LIB_STRINGBUILDER_H
+#define CPP_LIB_STRINGBUILDER_H
+
+namespace cppstdlib {
+  class StringBuilder {
+  public:
+    StringBuilder();
+
+  private :
+    char* theString;
+  }
+}
+
+#endif // CPP_LIB_STRINGBUILDER_H
