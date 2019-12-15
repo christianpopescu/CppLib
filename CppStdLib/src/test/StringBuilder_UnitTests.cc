a1fe772917803ec9c793a710ac63a6de5bd3c5c7
Christian Popescu <christian.popescu@outlook.com>
2019-12-15 17:00:56 +0100

    String builder 2nd test

--- CppStdLib/src/test/StringBuilder_UnitTests.cc
+++ CppStdLib/src/test/StringBuilder_UnitTests.cc
@@ -5,10 +5,23 @@ namespace {
   // Test default constructor
   TEST(stringBuilder, DefaultContructor) {
     cppstdlib::StringBuilder sb;
-    //EXPECT_STREQ("", sb.getString());
+    EXPECT_EQ("", sb.getString());
 
     EXPECT_EQ(0, sb.getLength());
 
     EXPECT_EQ(16, sb.getSize());
   }
+  
+  TEST(stringBuilder, Append) {
+    cppstdlib::StringBuilder sb;
+
+    sb.append("test");
+    
+    EXPECT_EQ("test", sb.getString());
+
+    EXPECT_EQ(4, sb.getLength());
+
+    EXPECT_EQ(16, sb.getSize());
+  }
+  
 }
