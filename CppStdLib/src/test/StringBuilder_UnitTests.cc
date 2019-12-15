6cc22d70a63a02daaa9db040842ff926f158185c
Christian Popescu <christian.popescu@outlook.com>
2019-12-15 17:13:57 +0100

    String Builder with 6 unit tests

--- CppStdLib/src/test/StringBuilder_UnitTests.cc
+++ CppStdLib/src/test/StringBuilder_UnitTests.cc
@@ -23,5 +23,62 @@ namespace {
 
     EXPECT_EQ(16, sb.getSize());
   }
+
+ TEST(stringBuilder, Append_2strings) {
+    cppstdlib::StringBuilder sb;
+
+    sb.append("test");
+    sb.append("test");
+    
+    EXPECT_EQ("testtest", sb.getString());
+
+    EXPECT_EQ(8, sb.getLength());
+
+    EXPECT_EQ(16, sb.getSize());
+  }
+
+ TEST(stringBuilder, Append_3strings_no_resize) {
+    cppstdlib::StringBuilder sb;
+
+    sb.append("test");
+    sb.append("test");
+    sb.append("xyzw123");
+    
+    EXPECT_EQ("testtestxyzw123", sb.getString());
+
+    EXPECT_EQ(15, sb.getLength());
+
+    EXPECT_EQ(16, sb.getSize());
+  }
+ TEST(stringBuilder, Append_4strings_resize) {
+    cppstdlib::StringBuilder sb;
+
+    sb.append("test");
+    sb.append("test");
+    sb.append("xyzw123");
+    sb.append("A");
+    
+    EXPECT_EQ("testtestxyzw123A", sb.getString());
+
+    EXPECT_EQ(16, sb.getLength());
+
+    EXPECT_EQ(32, sb.getSize());
+  }
   
+ TEST(stringBuilder, Append_5strings_resize) {
+    cppstdlib::StringBuilder sb;
+
+    sb.append("test");
+    sb.append("test");
+    sb.append("xyzw123");
+    sb.append("A");
+    sb.append("1234567890123456");
+    
+    EXPECT_EQ("testtestxyzw123A1234567890123456", sb.getString());
+
+    EXPECT_EQ(32, sb.getLength());
+
+    EXPECT_EQ(64, sb.getSize());
+  }
+
 }
